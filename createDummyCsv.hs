e6ee528966e9690e5706f91fd5dc0378b23d7785
cs8433 <christian.hoerauf@consorsbank.de>
2015-11-12 18:08:04 +0100

    Test generator works - use it with > to write to file

--- createDummyCsv.hs
+++ createDummyCsv.hs
@@ -1,45 +1,48 @@
 import System.Environment
 import System.Random
 import System.Time
+import Data.List
+
+type Row = [String]
+type Col = [String]
+type Range = Int
 
 main = do
    args <- getArgs
    now <- getClockTime
    clt <- toCalendarTime now
 
-   putStrLn $ show $ ctPicosec clt
    let gen = mkStdGen $ fromInteger $ ctPicosec clt
        (cols, rows) = parseParams args
-       -- rnd = randoms gen :: [Int]
 
-   putStrLn $ show $ fst $ createRandomRow gen cols
-   -- putStrLn $ show (take 5 rnd)
-   -- putStrLn $ show (take 5 (drop 5 rnd))
-   -- let (cols, rows) = parseParams args
-   -- putStrLn $ dummy cols rows
-   --
-   --
+   putStrLn $ csvsifyTable $ createRandomTable gen rows cols
+
 createRandomWord :: StdGen -> Int -> (String, StdGen)
 createRandomWord gen range = (take n (randomRs ('A', 'z') g), g)
    where (n, g) = randomR (0,range) gen :: (Int, StdGen)
 
-createRandomRow :: StdGen -> Int -> ([String], StdGen)
+createRandomRow :: StdGen -> Int -> (Row, StdGen)
 createRandomRow gen maxLength = randomList g n
    where (n, g) = randomR (0, maxLength) gen :: (Int, StdGen)
          randomList gl 0 = ([], gl)
          randomList gl n = (word : (fst $ randomList gw $ n-1), gw)
             where (word, gw) = createRandomWord gl 20
 
--- createRandomTable :: StdGen -> Int -> Int -> String
--- createRandomTable gen cols rows = unlines
+createRandomTable :: StdGen -> Int -> Int -> [Row]
+createRandomTable _ 0 _ = []
+createRandomTable gen rows cols = r : rs
+   where (r, g) = createRandomRow gen cols
+         rs = createRandomTable g (rows-1) cols
+
+csvsifyTable :: [Row] -> String
+csvsifyTable rs = unlines (map (intercalate ";") rs)
 
 dummy ::  Int -> Int -> String
 dummy c r = (show c) ++ (show r)
 
--- createTable :: Int -> Int -> String
--- createTable c r = show $ createRow c
---
--- createRow :: Int -> [Int]
+-- createTable :: Int ->) Int -> String
+-- createTable c r = show $ createRow cDjkh--
+-- createRow :: Int -> [Int])
 -- createRow c = take c $ randoms $ mkStdGen c :: Int
 --
 parseParams :: [String] -> (Int, Int)
