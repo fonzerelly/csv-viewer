30f70d0b70400ce93741e6b0b912ccb1d7c5c922
cs8433 <christian.hoerauf@consorsbank.de>
2015-11-14 02:30:40 +0100

    Truncate String to console size

--- csv-viewer.hs
+++ csv-viewer.hs
@@ -1,8 +1,20 @@
 import CsvViewer.Core
 import System.Environment
+import System.Console.Terminal.Size
+import Data.Maybe
+import Data.List
 
 main = do
    params <- getArgs
+   s <- size
    let csvFile = head params
+       consoleWidth = width $ fromJust s
+       consoleHeight = height $ fromJust s
    contents <- readFile csvFile
-   putStrLn (displayCsv contents)
+
+   let rows = transformTable $ splitIntoRows contents
+       visibleRows = map (intercalate "|") $ take consoleHeight rows
+       truncatedRows = map (take (consoleWidth-1)) visibleRows
+       screen =  unlines truncatedRows
+
+   putStrLn screen
