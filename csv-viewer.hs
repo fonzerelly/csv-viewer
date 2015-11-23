a36338c14ba1c6883fa731e9d22a5e4a344fd2cd
cs8433 <christian.hoerauf@consorsbank.de>
2015-11-23 09:11:54 +0100

    Basic Scrolling is working

--- csv-viewer.hs
+++ csv-viewer.hs
@@ -1,20 +1,45 @@
 import CsvViewer.Core
+import CsvViewer.Keybindings
+
 import System.Environment
 import System.Console.Terminal.Size
+import System.Console.ANSI
 import Data.Maybe
 import Data.List
 
+main :: IO ()
 main = do
    params <- getArgs
-   s <- size
    let csvFile = head params
-       consoleWidth = width $ fromJust s
-       consoleHeight = height $ fromJust s
    contents <- readFile csvFile
+   dispatchUserInput contents 0
 
-   let rows = transformTable $ splitIntoRows contents
-       visibleRows = map (intercalate "|") $ take consoleHeight rows
-       truncatedRows = map (take (consoleWidth-1)) visibleRows
-       screen =  unlines truncatedRows
 
-   putStrLn screen
+truncateOutput :: String -> (Int, Int) -> Int -> String
+truncateOutput contents console offset = unlines truncatedRows
+   where rows = transformTable $ splitIntoRows contents
+         visibleRows = map (intercalate "|") $ take ((snd console)-1) $ drop offset rows
+         truncatedRows = map (take ((fst console)-1)) visibleRows
+
+dispatchUserInput :: String -> Int-> IO ()
+dispatchUserInput contents firstRow = do
+   s <- size
+   let console = (width $ fromJust s, height $ fromJust s)
+       handleKey (-1) _ = 0
+       handleKey offset UP
+         | offset == 0 = 0
+         | otherwise = offset - 1
+       handleKey offset DOWN
+         | offset >= (length contents) - (snd console) = (length contents) - (snd console)
+         | otherwise = offset + 1
+   putStrLn $ truncateOutput contents console firstRow
+   putStrLn $ show firstRow
+   c <- catchKey
+   if c == QUIT
+      then return ()
+      else do
+         setCursorPosition 0 0
+         dispatchUserInput contents (handleKey firstRow c)
+
+
+
--- csv-viewer.spec.hs
+++ csv-viewer.spec.hs
@@ -2,6 +2,7 @@ import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
 import CsvViewer.Core
+import CsvViewer.Keybindings
 
 main::IO ()
 main = hspec $ do
@@ -74,3 +75,11 @@ main = hspec $ do
       it "renders pipes between equal sized columns" $ do
          displayCsv "Lorem;dolor;sit\nAmend;ganimet" `shouldBe` "Lorem| dolor |sit\nAmend|ganimet|   \n"
 
+
+   describe "convertKey" $ do
+      it "Converts getch-codes into proprietery types" $ do
+         convertKey 72 `shouldBe` UP
+         convertKey 80 `shouldBe` DOWN
+         convertKey 3  `shouldBe` QUIT
+         convertKey 4  `shouldBe` QUIT
+         convertKey 27 `shouldBe` QUIT
