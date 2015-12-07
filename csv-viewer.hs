bcab071901d381e141e6f352e9d1d1e31bcaef88
cs8433 <christian.hoerauf@consorsbank.de>
2015-12-07 08:23:06 +0100

    Use tuple instead of int

--- csv-viewer.hs
+++ csv-viewer.hs
@@ -13,25 +13,24 @@ main = do
    params <- getArgs
    let csvFile = head params
    contents <- readFile csvFile
-   dispatchUserInput (transformTable (splitIntoRows contents)) 0
+   dispatchUserInput (transformTable (splitIntoRows contents)) (0, 0)
 
-dispatchUserInput :: [Row] -> Int-> IO ()
-dispatchUserInput contents firstRow = do
+dispatchUserInput :: [Row] -> CellOffset -> IO ()
+dispatchUserInput contents offset = do
    s <- size
-   let console = (width $ fromJust s, height $ fromJust s)
-       w = resize (fromJust s) 0 (-1)
+   let w = resize (fromJust s) 0 (-1)
        handleKey (-1) _ = 0
        handleKey offset UP
          | offset == 0 = 0
          | otherwise = offset - 1
        handleKey offset DOWN
-         | offset >= (length contents) - (snd console) = (length contents) - (snd console)
+         | offset >= (length contents) - (height w) = (length contents) - (height w)
          | otherwise = offset + 1
-   putStrLn $ truncateTable contents (0, firstRow) w
+   putStrLn $ truncateTable contents offset w
    c <- catchKey
    if c == QUIT
       then return ()
       else do
          setCursorPosition 0 0
-         dispatchUserInput contents (handleKey firstRow c)
+         dispatchUserInput contents (0, (handleKey (snd offset) c))
 
