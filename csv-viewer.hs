3ffa6b930b4a86d12f3349a734d3edc4b152cbf5
cs8433 <christian.hoerauf@consorsbank.de>
2015-12-09 18:47:25 +0100

    Allows columnwise navigation

--- csv-viewer.hs
+++ csv-viewer.hs
@@ -19,18 +19,31 @@ dispatchUserInput :: [Row] -> CellOffset -> IO ()
 dispatchUserInput contents offset = do
    s <- size
    let w = resize (fromJust s) 0 (-1)
-       handleKey (-1) _ = 0
+       calcVisibleCols index = length visibleRowPart + (sum (map length visibleRowPart))
+         where visibleRowPart = init (drop index (head contents))
+
+       handleKey (col, -1) key
+         | key == UP   || key == DOWN  = (col,0)
+       handleKey ( -1, row) key
+         | key == LEFT || key == RIGHT = (0, row)
        handleKey offset UP
-         | offset == 0 = 0
-         | otherwise = offset - 1
+         | (snd offset) == 0 = (fst offset, 0)
+         | otherwise = ((fst offset), (snd offset) - 1)
        handleKey offset DOWN
-         | offset >= (length contents) - (height w) = (length contents) - (height w)
-         | otherwise = offset + 1
+         | (snd offset) >= (length contents) - (height w) = (fst offset, (length contents) - (height w))
+         | otherwise = ((fst offset), (snd offset) + 1)
+       handleKey offset RIGHT
+         | calcVisibleCols (fst offset) > (width w) = ((fst offset + 1), (snd offset))
+         | otherwise = offset
+       handleKey offset LEFT
+         | (fst offset) == 0 = (0, (snd offset))
+         | otherwise = ((fst offset - 1), (snd offset))
+
    putStrLn $ truncateTable contents offset w
    c <- catchKey
    if c == QUIT
       then return ()
       else do
          setCursorPosition 0 0
-         dispatchUserInput contents (0, (handleKey (snd offset) c))
+         dispatchUserInput contents (handleKey offset c)
 
